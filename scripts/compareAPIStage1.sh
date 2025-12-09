238ee2f5b931ad03429ed237c9dd8f688516b30c
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-12-09 13:15:24 +0800

    test ci

--- scripts/compareAPIStage1.sh
+++ scripts/compareAPIStage1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd "../"
-
 find . -maxdepth 2 -name "*-api" -type d | while read apiModule; do
     find "$apiModule/src/main/java" -type f -name "*.java" -exec md5sum {} \;
 done | md5sum | cut -d' ' -f1 > api_md5.txt
--- scripts/pushAPIVersionInfo.sh
+++ scripts/pushAPIVersionInfo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API_VERSION=$(cat ../api_md5.txt)
+API_VERSION=$(cat api_md5.txt)
 
 echo "Running client_send_stage2.py..."
 python client_send_stage2.py   --token "${PUSH_TOKEN}" \
